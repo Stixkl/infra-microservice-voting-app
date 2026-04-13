3446229b5235dfd3ac198862e85319f6a5ab5363
Daniel Gonzalez <danielgonzalezrivera2002@gmail.com>
2026-04-12 21:17:24 -0500

    fix: pipline

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,7 +10,7 @@ variable "project_name" {
 
 variable "environment" {
   type    = string
-  default = "shared"
+  default = "dev"
 }
 
 variable "service_images" {
@@ -43,4 +43,3 @@ variable "kafka_image" {
   type    = string
   default = "bitnami/kafka:3.7"
 }
-
